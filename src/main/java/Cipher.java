1e59f39cb586ab3e7d65c03e949ab9e616f9fb68
kinyua254 <victor.mutugi@student.moringaschool.com>
2022-02-23 21:25:25 +0300

    tests doing good

---
+++ src/main/java/Cipher.java
@@ -0,0 +1,25 @@
+public class Cipher {
+
+    private String inputString;
+    private int cipherKey;
+
+    public String getInputString() {
+
+        return inputString;
+    }
+
+    public void setInputString(String inputString) {
+
+        this.inputString = inputString;
+    }
+
+    public int getCipherKey() {
+
+        return cipherKey;
+    }
+
+    public void setCipherKey(int cipherKey) {
+
+        this.cipherKey = cipherKey;
+    }
+}
