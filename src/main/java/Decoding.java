2e831011881fc7b31f7768233f9145c2895738ef
kinyua254 <victor.mutugi@student.moringaschool.com>
2022-02-23 03:50:34 +0300

    code on the coding and decoding

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,5 +1,37 @@
 public class Decoding {
-    public static void main(String[] args) {
+
+
+    public String decode(String inputString, int cipherKey) {
+        String decodedCipher = "";
+        //Converting the string to an array of characters
+        for (int i = 0; i < inputString.length(); i++) {
+            char singleChar = inputString.charAt(i);
+            //Check if the Character is a symbol or a letter
+            if (Character.isLetter(singleChar)) {
+
+                // LowerCase
+                if (Character.isLowerCase(singleChar)) {
+                    char character = (char) (singleChar - cipherKey);
+
+                    decodedCipher += character;
+
+
+                    //UpperCase
+                } else if (Character.isUpperCase(singleChar)) {
+                    char character = (char) (singleChar - cipherKey);
+
+                    decodedCipher += character;
+                }
+
+
+            } else {
+                decodedCipher += singleChar;
+            }
+        }
+
+
+        return decodedCipher;
+
 
     }
 }
--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,5 +1,37 @@
 public class Encoding {
-    public static void main(String[] args) {
+
+
+    public String encode(String inputString, int cipherKey) {
+
+        String encodedCipher = "";
+        //Converting the string to an array of characters
+        for (int i = 0; i < inputString.length(); i++) {
+            char singleChar = inputString.charAt(i);
+            //Check if the Character is a symbol or a letter
+            if (Character.isLetter(singleChar)) {
+
+                // LowerCase
+                if (Character.isLowerCase(singleChar)) {
+                    char character = (char) (singleChar + cipherKey);
+                    encodedCipher += character;
+
+                    //UpperCase
+                } else if (Character.isUpperCase(singleChar)) {
+                    char character = (char) (singleChar + cipherKey);
+                    encodedCipher += character;
+
+                }
+
+            } else {
+                encodedCipher += singleChar;
+            }
+        }
+
+
+        return encodedCipher;
+
 
     }
+
+
 }
